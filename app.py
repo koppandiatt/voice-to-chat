892956bc66620dc79e78ffe5b74370dc2dfcb1fa
Koppandi Alin <alin@project-lary.com>
2023-10-17 16:24:50 +0300

    add cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 # Configuration (Replace with your OpenAI API key)
 OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY'
