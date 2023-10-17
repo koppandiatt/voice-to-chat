73baa99223afcbfdceed26534cdc85edb5d7c4ff
Koppandi Alin <koppandi.alin@gmail.com>
2023-10-17 16:16:00 +0300

    v2

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 # Configuration (Replace with your OpenAI API key)
 OPENAI_API_KEY = 'YOUR_OPENAI_API_KEY'
 
+
 @app.route('/voice_to_chat', methods=['POST'])
 def voice_to_chat():
     # Check if the post request has the file part
     if 'file' not in request.files:
         return jsonify(error='No file part'), 400
-    
+
     file = request.files['file']
 
     # Check if user did not select file
@@ -28,12 +29,17 @@ def voice_to_chat():
     }
 
     # Note: Update the API endpoint to the correct Whisper API endpoint
-    response = requests.post('https://api.openai.com/v1/whisper/voice-to-text', headers=headers, data=file.read())
+    response = requests.post(
+        'https://api.openai.com/v1/whisper/voice-to-text',
+        headers=headers,
+        data=file.read()
+    )
 
     if response.status_code != 200:
         return jsonify(error='Failed to transcribe audio'), 500
 
     return jsonify(text=response.json()['text'])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
